c06b8ca35c5a39250de705c39842e2c420f98b76
Chedvy-Rizi <c0556734708@gmail.com>
2026-05-01 17:10:10 +0300

    adding imports

--- src/main/java/com/example/demo/controller/ManagerController.java
+++ src/main/java/com/example/demo/controller/ManagerController.java
@@ -11,6 +11,8 @@ import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
